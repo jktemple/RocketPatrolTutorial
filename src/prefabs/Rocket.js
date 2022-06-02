dee0567170d939beada874e45189b947b49aa276
jktemple <jktemple@ucsc.edu>
2022-06-01 21:57:22 -0700

    Debugging

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -19,6 +19,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
             this.sfxRocket.play();
+            console.log("Fired");
         }
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
             this.y -= this.moveSpeed;
